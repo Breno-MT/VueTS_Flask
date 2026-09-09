ba77514015127a49ca67331e8d217c79bb0d2b83
Breno-MT <brenomtdev@gmail.com>
2026-09-09 12:13:36 -0300

    REFACTOR: Exibição completa de data

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -85,6 +85,9 @@ function formatarData(iso: string): string {
   return new Date(iso).toLocaleDateString('pt-BR', {
     day: '2-digit',
     month: 'short',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
   })
 }
 
